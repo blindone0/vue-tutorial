d685e4c0a27fa1e083d6ec0094237336a9015246
Ihor <mrsettle1@gmail.com>
2021-08-13 12:58:53 +0300

    emmiting reversed props

--- src/App.vue
+++ src/App.vue
@@ -29,8 +29,8 @@ export default {
         }
     },
     methods: {
-        createPost(post, second, third) {
-            console.log(post, second, third);
+        createPost(post) {
+            this.posts.push(post)
         }
     }
     
