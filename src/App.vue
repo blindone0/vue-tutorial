ece344fcf7fbd6223e763c632ab706f79505a430
Ihor <mrsettle1@gmail.com>
2021-08-13 11:49:58 +0300

    add new post fun. @submit.prevent, v-bind:value=title. push new data from function

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="app">
         <div>
-            <form>
+            <form @submit.prevent>
                 <h4>Post creation</h4>
                 <input v-bind:value="title" @input="title = $event.target.value" class="input" type="text" placeholder="Name">
                 <input v-bind:value="body" @input="body = $event.target.value" class="input" type="text" placeholder="Description">
@@ -32,7 +32,12 @@ export default {
     },
     methods: {
         createPost() {
-
+            const newPost = {
+                id : Date.now(),
+                title: this.title,
+                body: this.body,
+            }
+            this.posts.push(newPost);
         }
     }
     
