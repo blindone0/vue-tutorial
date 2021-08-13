4b2e9978a3c30172fb9748d2c8c4d0239c959d9e
Ihor <mrsettle1@gmail.com>
2021-08-13 10:44:36 +0300

    New field methods with 2 functions that incerements like and dislike filds

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        <div>Likes count {{ likes }}</div>        
+        <div>
+            <button>Like</button>
+            <button>Dislike</button>
+        </div>
+        <div>Likes count {{ likes }}</div>
+        <div>Likes count {{ dislikes }}</div>             
     </div>
 </template>
 
@@ -10,7 +15,16 @@ export default {
     data() {
         return {
             likes: 0,
+            dislikes: 0,
 
+        }
+    },
+    methods: {
+        addLike() {
+            this.likes +=1;
+        },
+        addDislike() {
+            this.likes +=1;
         }
     }
     
